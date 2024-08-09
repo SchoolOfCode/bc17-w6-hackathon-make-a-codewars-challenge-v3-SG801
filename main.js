cb57cec83c9f7ce7c29cbb16b234415b2a35d077
Shilpa <eclectic89@hotmail.com>
2024-08-09 09:20:28 +0100

    Fixed code

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-const arr = ['a', 'e', 'i', 'o', 'u']
-export function countVowel(str) {
+const arr = ['a', 'e', 'i', 'o', 'u','A', 'E', 'I', 'O', 'U'];
+function countVowel(str) {
     let counter = 0;
     if (str.length == 0 || str.length > 8) {
-        console.log('error', 'enter a 8 or less character word')
-
+        console.log('error', 'enter a 8 or less character word')}
+else
+{
         for (let i = 0; i < str.length; i++) {
             if (arr.includes(str[i])) {
                 counter++;
-                return counter;
+                
 
             }
         }
+        return counter;
+        
 
     }
-
-
-
 }
-console.log(countVowel('code'));
+console.log(countVowel('codennnfnfn'));
