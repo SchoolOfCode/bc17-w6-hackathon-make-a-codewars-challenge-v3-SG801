ee528fc0e9029b5885f7774d4386168290202125
Shilpa <eclectic89@hotmail.com>
2024-08-09 10:41:54 +0100

    main file and test file added

--- main.test.js
+++ main.test.js
@@ -7,13 +7,16 @@ test('count the number of vowels in a string', async () => {
     const testCase = [
         { str: 'Coding', expectedCount: 2 },
         { str: 'Hijango', expectedCount: 3 },
-        { str: 'CodeNinja', error: 'enter a 8 or less character word' },];
+        { str: 'CodeNinja', expectedCount: null, error: 'enter a 8 or less character word' },];
     for (const { str, expectedCount, error } of testCase) {
+        if (error) {
+            expect(() => countVowel(str)).toThrow(error);
+        } else {
 
-        const actualCount = countVowel(str);
-        expect(actualCount).toBe(expectedCount);
-        console.log(`Test for word "${word}": expected ${expectedCount}, got ${result}`);
-
+            const actualCount = countVowel(str);
+            expect(actualCount).toBe(expectedCount);
+            console.log(`Test for word "${str}": expected ${expectedCount}, got ${result}`);
+        }
     }
 
 
